fe68525244f86f5fe107106388d2c8dd8d375bc7
gwilson253 <gwilson253@gmail.com>
2017-12-09 10:31:25 -0800

    12a

--- manage.py
+++ manage.py
@@ -3,14 +3,15 @@ import os
 from app import create_app, db
 from flask_script import Manager, Shell
 from flask_migrate import Migrate, MigrateCommand
-from app.models import User, Role, Permission, Post
+from app.models import User, Follow, Role, Permission, Post
 
 app = create_app(os.getenv('APP_NAME_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app, db)
 
 def make_shell_context():
-    return dict(app=app, db=db, User=User, Role=Role, Permission=Permission, Post=Post)
+    return dict(app=app, db=db, User=User, Follow=Follow, Role=Role,
+                Permission=Permission, Post=Post)
 
 manager.add_command("shell", Shell(make_context=make_shell_context))
 manager.add_command('db', MigrateCommand)
